a34a6a7e5f3685ea44ed39691370cf7789cf43fa
29ravikant-akash <29ravikant2017@gmail.com>
2021-11-06 15:00:36 +0530

    2nd commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(
 );
 
 // app.use(express.json());
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 app.get("/myacc",async (req,res)=>{
     console.log("requested");
@@ -51,7 +51,7 @@ app.get("/myacc",async (req,res)=>{
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 app.get("/*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"build/index.html"));
+    res.sendFile(path.join(__dirname,"client/build/index.html"));
 });
 
 
